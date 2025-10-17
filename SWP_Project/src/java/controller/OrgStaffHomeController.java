e43256cde95762b25567088d5642eecc54c0b2b4
Thanhcocodo <nhthanh0611@gmail.com>
2025-10-17 11:06:43 +0700

    Add: controller: OrgStaffHomeController

---
+++ SWP_Project/src/java/controller/OrgStaffHomeController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "OrgStaffHomeController", urlPatterns = {"/orgstaff/home"})
+public class OrgStaffHomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/orgstaff/home.jsp").forward(request, response);
+    }
+}
