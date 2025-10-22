e586c31f9a95e29de01bcf77d8b9f59d9871b002
Thanhcocodo <nhthanh0611@gmail.com>
2025-10-22 17:32:57 +0700

    Update web folder from mainV1

--- SWP_Project/src/java/filter/OrgStaffFilter.java
+++
@@ -1,9 +0,0 @@
-package filter;
-
-import jakarta.servlet.annotation.WebFilter;
-
-@WebFilter(urlPatterns = {"/orgstaff/*"})
-public class OrgStaffFilter extends BaseRoleFilter {
-    @Override
-    protected String expectedRole() { return "OrgStaff"; }
-}
--- SWP_Project/src/java/filter/OrganizationFilter.java
+++
@@ -1,9 +0,0 @@
-package filter;
-
-import jakarta.servlet.annotation.WebFilter;
-
-@WebFilter(urlPatterns = {"/organization/*"})
-public class OrganizationFilter extends BaseRoleFilter {
-    @Override
-    protected String expectedRole() { return "Organization"; }
-}
